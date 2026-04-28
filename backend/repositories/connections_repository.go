32c2145292d15310c1ec0141e6855fcd29b703da
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-04-28 20:45:36 -0300

    feat(repositories): adiciona suporte para o campo ColorHex nas funções Create e Update do ConnectionsRepository

--- backend/repositories/connections_repository.go
+++ backend/repositories/connections_repository.go
@@ -46,6 +46,7 @@ func (r *ConnectionsRepository) Create(ctx context.Context, input models.CreateC
 		Username: input.Username,
 		Password: input.Password,
 		SSL:      input.SSL,
+		ColorHex: input.ColorHex,
 	}
 
 	err := r.db.WithContext(ctx).Create(&connection).Error
@@ -54,5 +55,58 @@ func (r *ConnectionsRepository) Create(ctx context.Context, input models.CreateC
 	}
 
 	connection.Driver = driver
+	return connection, nil
+}
+
+func (r *ConnectionsRepository) Update(ctx context.Context, input models.UpdateConnectionInput) (models.Connection, error) {
+	var connection models.Connection
+	if err := r.db.WithContext(ctx).Preload("Driver").First(&connection, input.ID).Error; err != nil {
+		return models.Connection{}, err
+	}
+
+	updates := map[string]interface{}{}
+
+	if input.Name != nil {
+		updates["name"] = *input.Name
+	}
+
+	if input.Host != nil {
+		updates["host"] = *input.Host
+	}
+
+	if input.Port != nil {
+		updates["port"] = *input.Port
+	}
+
+	if input.Database != nil {
+		updates["database_name"] = *input.Database
+	}
+
+	if input.Username != nil {
+		updates["username"] = *input.Username
+	}
+
+	if input.Password != nil {
+		updates["password"] = *input.Password
+	}
+
+	if input.SSL != nil {
+		updates["ssl"] = *input.SSL
+	}
+
+	if input.ColorHex != nil {
+		updates["color_hex"] = *input.ColorHex
+	}
+
+	if len(updates) > 0 {
+		if err := r.db.WithContext(ctx).Model(&connection).Updates(updates).Error; err != nil {
+			return models.Connection{}, err
+		}
+	}
+
+	if err := r.db.WithContext(ctx).Preload("Driver").First(&connection, input.ID).Error; err != nil {
+		return models.Connection{}, err
+	}
+
 	return connection, nil
 }
