60a40508e0151eccaee27c6ecd1668a0fe1cc840
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-04-28 21:08:23 -0300

    fix(main): corrige o título da aplicação para "Data Forge Studio"

--- main.go
+++ main.go
@@ -17,9 +17,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "data-forge-studio",
-		Width:  1024,
-		Height: 768,
+		Title:     "Data Forge Studio",
+		Width:     1024,
+		Height:    768,
+		Frameless: true,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
